4d73c9410b09e8d10a9aa85bdd257590c1d70c12
Sabrine <Sabrine.RIAHI@student.umons.ac.be>
2017-10-13 16:25:14 +0200

    Use JUnit 4 instead of 5.

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,20 @@
 package exercise1;
 
 class Person {
+
+	private String name ;
+	private int age ;
+	// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
+	public Person(String name, int age) {
+		this.name=name;
+		this.age=age;
+	}
+
+	// Créez des getters publics pour lire ces valeurs une fois la classe construite.
+	public String getName () {
+		return name;
+	}
+	public int getAge () {
+		return age;
+	}
 }
